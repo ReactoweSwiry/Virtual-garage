6aec193ae661a7bfaeca5ef29300bb1c4f962859
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2024-11-18 14:12:42 +0100

    tailwind css, native wind, native paper, prettier

--- virtual-garage/app/(tabs)/index.tsx
+++ virtual-garage/app/(tabs)/index.tsx
@@ -1,31 +1,23 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Button } from 'react-native-paper'
+import { Text, View } from '../../components/Themed'
+import { Alert } from 'react-native'
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
-}
+    <View className='flex-1 items-center justify-center border-2 border-cyan-200'>
+      <Text className='p-4 '>React Expo preconfigured with:</Text>
+      <Text className='pt-3'>Tailwinds (NativeWind)</Text>
+      <Text className='pt-3'>React Native Paper (Material UI)</Text>
+      <Text className='pt-3'>Prettier</Text>
+      <View lightColor='#eee' darkColor='rgba(255,255,255,0.1)' />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+      <Button
+        className='mt-20'
+        mode='contained'
+        onPress={() => Alert.alert('Paper button is working!')}
+      >
+        Paper button
+      </Button>
+    </View>
+  )
+}
