7593a1470a202eb9ee8157b9481d68a9486e9194
kayYZ1 <overek7133@gmail.com>
2024-11-25 14:36:31 +0100

    feat: settings page layout

--- client/app/(tabs)/settings.tsx
+++ client/app/(tabs)/settings.tsx
@@ -1,27 +1,98 @@
-import { Fragment } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+import SettingsSection from '../components/SettingsSection';
+import SettingItem from '../components/SettingsItem';
 
 export default function Settings() {
 	return (
-		<Fragment>
+		<ParallaxScrollView
+			headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+			headerImage={
+				<Image
+					source={require('@/assets/images/partial-react-logo.png')}
+					style={styles.reactLogo}
+				/>
+			}>
 			<View style={styles.titleContainer}>
-				<Text>Welcome!</Text>
+				<Text style={styles.title}>Settings</Text>
+				<Text style={styles.description}>
+					Here is your settings panel, tweak it as you like.
+				</Text>
 			</View>
-			<View style={styles.stepContainer}>
-				<Text>Settings app</Text>
-			</View>
-		</Fragment>
+
+			<SettingsSection title='Account'>
+				<SettingItem
+					icon='person-circle'
+					title='Profile'
+				/>
+				<SettingItem
+					icon='notifications'
+					title='Notifications'
+				/>
+				<SettingItem
+					icon='lock-closed'
+					title='Privacy'
+				/>
+			</SettingsSection>
+
+			<SettingsSection title='Preferences'>
+				<SettingItem
+					icon='color-palette'
+					title='Appearance'
+				/>
+				<SettingItem
+					icon='globe'
+					title='Language'
+				/>
+				<SettingItem
+					icon='time'
+					title='Time Zone'
+				/>
+			</SettingsSection>
+
+			<SettingsSection title='Support'>
+				<SettingItem
+					icon='help-circle'
+					title='Help Center'
+				/>
+				<SettingItem
+					icon='document-text'
+					title='Terms of Service'
+				/>
+				<SettingItem
+					icon='shield-checkmark'
+					title='Privacy Policy'
+				/>
+			</SettingsSection>
+
+			<SettingsSection title='App Info'>
+				<SettingItem
+					icon='information-circle'
+					title='Version'
+				/>
+			</SettingsSection>
+		</ParallaxScrollView>
 	);
 }
 
 const styles = StyleSheet.create({
 	titleContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		gap: 8,
+		flexDirection: 'column',
+		gap: 4,
 	},
-	stepContainer: {
-		gap: 8,
-		marginBottom: 8,
+	title: {
+		fontSize: 16,
+		fontWeight: 'bold',
+	},
+	description: {
+		fontSize: 14,
+		fontWeight: 300,
+	},
+	reactLogo: {
+		height: 178,
+		width: 290,
+		bottom: 0,
+		left: 0,
 	},
 });
