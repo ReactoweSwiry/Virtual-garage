5513505d98c306f335992dd68becd38212a97f9e
kayYZ1 <overek7133@gmail.com>
2024-12-04 23:03:32 +0100

    feat: add new car form view

--- client/app/(tabs)/(garage)/new-car.tsx
+++ client/app/(tabs)/(garage)/new-car.tsx
@@ -1,5 +1,26 @@
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
+import {
+	View,
+	Text,
+	TextInput,
+	StyleSheet,
+	Image,
+	TouchableOpacity,
+	Alert,
+} from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import Button from '@/components/ui/Button';
+
+async function PickFile() {
+	try {
+		const result = await DocumentPicker.getDocumentAsync({});
+		console.log(result);
+	} catch (err) {
+		Alert.alert('Error', 'Failed to pick a file');
+	}
+}
 
 export default function NewCar() {
 	return (
@@ -9,11 +30,44 @@ export default function NewCar() {
 				<Image source={require('@/assets/images/partial-react-logo.png')} />
 			}>
 			<View style={styles.titleContainer}>
-				<Text style={styles.title}>Welcome to your virtual garage!</Text>
-				<Text style={styles.description}>Here is your collection of cars</Text>
+				<Text style={styles.title}>Here you can add your car</Text>
+				<Text style={styles.description}>
+					Make sure to fill all the important for you information!
+				</Text>
 			</View>
-			<View style={styles.content}>
-				<Text>Here form</Text>
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.fileUpload}
+					onPress={PickFile}>
+					<MaterialIcons
+						name='upload-file'
+						size={40}
+						color='#007bff'
+					/>
+					<Text style={styles.fileUploadText}>Upload car image</Text>
+				</TouchableOpacity>
+				<View style={styles.dividerContainer}>
+					<View style={styles.divider} />
+					<Text style={styles.dividerText}>or</Text>
+					<View style={styles.divider} />
+				</View>
+				<Text style={styles.additionalText}>
+					We will pick an icon that correspondts to the type of your vehicle!
+				</Text>
+				<TextInput
+					style={styles.input}
+					placeholder='Name'
+				/>
+				<TextInput
+					style={styles.input}
+					placeholder='Model'
+				/>
+				<TextInput
+					style={styles.input}
+					placeholder='Year'
+					keyboardType='numeric'
+				/>
+				<Button title='Save' />
 			</View>
 		</ParallaxScrollView>
 	);
@@ -32,11 +86,52 @@ const styles = StyleSheet.create({
 		fontSize: 14,
 		fontWeight: 300,
 	},
-	content: {
+	container: {
+		flex: 1,
+		padding: 20,
+		justifyContent: 'center',
+	},
+	fileUpload: {
+		borderWidth: 2,
+		borderColor: '#007bff',
+		borderStyle: 'dashed',
+		borderRadius: 5,
+		padding: 20,
+		marginBottom: 15,
+		alignItems: 'center',
+	},
+	fileUploadText: {
+		color: '#007bff',
+		fontSize: 14,
+		fontWeight: 'semibold',
+	},
+	dividerContainer: {
 		flexDirection: 'row',
-		flexWrap: 'wrap',
-		justifyContent: 'space-between',
-		paddingTop: 8,
-		paddingBottom: 4,
+		alignItems: 'center',
+	},
+	divider: {
+		flex: 1,
+		height: 1,
+		backgroundColor: 'black',
+		opacity: 0.25,
+	},
+	dividerText: {
+		paddingHorizontal: 10,
+		opacity: 0.5,
+		fontSize: 14,
+	},
+	additionalText: {
+		fontSize: 14,
+		textAlign: 'center',
+		opacity: 0.75,
+		paddingVertical: 20,
+	},
+	input: {
+		padding: 10,
+		borderRadius: 5,
+		marginBottom: 15,
+	},
+	button: {
+		textTransform: 'capitalize',
 	},
 });
