1f66f2fd9110e53f2b093a1434de4f0a1fb5e07e
Kacper <overek7133@gmail.com>
2024-12-05 10:23:39 +0100

    fix: add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from .database import engine
 from .routes import routes
@@ -8,6 +9,7 @@ from .models import Base
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     routes(app)
 
     Base.metadata.create_all(engine)
