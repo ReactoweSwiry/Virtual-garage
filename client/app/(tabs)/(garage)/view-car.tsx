5a3f82f22a13f1ab7cfc86e0bcd3e667f4712549
kayYZ1 <overek7133@gmail.com>
2025-01-13 12:55:26 +0100

    ci(test): test branch protection rule

--- client/app/(tabs)/(garage)/view-car.tsx
+++ client/app/(tabs)/(garage)/view-car.tsx
@@ -1,7 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { router, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { View, StyleSheet, ScrollView, Image } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+  ScrollResponderEvent,
+} from 'react-native';
 import {
   Text,
   ActivityIndicator,
